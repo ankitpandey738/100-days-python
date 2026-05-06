6dc1e203a5f0bdfc8d155fd164bdaca1c129e9ca
ankitpandey738 <theunfinishedride@gmail.com>
2026-05-06 10:24:58 +0530

    Ist commit

--- 25Tuples.py
+++ 25Tuples.py
@@ -56,3 +56,66 @@ print(T3)
 T4 =(1,2,3,(4,5))
 print(T4)
 print(T4[-1][0])
+
+
+#Difference btw List and Tuple is in edit
+
+# Edit  
+#List : List is mutable means if a list has been created and we want to edit a item inside the list then we csn do it easily.
+L = [1,2,3,4,5]
+L[0] = 100
+print(L)
+
+#Tuple : Means if a tuple has been created then we cannot edit the item's that is present inside the tuple.
+'''
+T2
+T2[0]=100
+print(T2)'''
+#Tuple just like strings are immutable.
+# We cannot add any new items in the existing tuples because as we know it tuple is immutable.
+
+# Deleting Tuple
+'''
+T1
+del T1
+print(T1)
+'''
+'''
+We cannot delete a single item inside a tuple because one item deletion in tuple means overall data is being changed due to this we cannot delete a part of a tuple .
+T2
+del T2[-1]
+print(T2)
+'''
+
+
+# Operations that are same as it is available for list.
+T2
+T3
+T7 = T2+T3
+T8= T2*3
+print(T7)
+print(T8)
+for i in T2:
+    print(i)
+    
+L= 1 in T2
+print(L)
+
+
+# Functions are also same as it is available for list.
+
+A = len(T2)
+print(A)
+b = min(T2)
+print(b)
+c=max(T2)
+print(c)
+d = sum(T2)
+print(d)
+e= sorted(T2)
+print(e)
+f=sorted(T2,reverse=True)
+print(f)
+
+# Tuples are read-only data type but it doesn't have the write ability like a list has the write ability as well as read ability.
+
