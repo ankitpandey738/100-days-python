f8db3682d4d0da3a912bf3473fee1317e03e81b6
ankitpandey738 <theunfinishedride@gmail.com>
2026-05-06 10:42:02 +0530

    Ist commit

---
+++ 26Sets.py
@@ -0,0 +1,17 @@
+#Sets
+
+'''
+4 Rules about Sets:
+1: Sets do not allow duplicates.
+2: Sets have no indexing/slicing
+3: Sets dont allow mutable data types
+4: Set itself is a mutable data types means a set cannot have another set inside the set.
+'''
+
+
+# Createing a Set
+
+# Empty set
+S1={}
+a=type(S1)
+print(a)
